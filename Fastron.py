beb78e97b0d7659aa57e4517daf612f42e00e799
Yuheng Zhi <770606451@qq.com>
2020-05-11 19:23:20 -0700

    MultiFastron, SVM added

--- Fastron.py
+++ Fastron.py
@@ -1,5 +1,8 @@
 import numpy as np
 from matplotlib import pyplot as plt
+import torch
+from sklearn.svm import SVC
+from scipy import ndimage
 
 class Obstacle:
     def __init__(self, kind, position, size):
@@ -35,19 +38,36 @@ class Fastron(CollisionChecker):
     def __init__(self, obstacles, gt_checker=None):
         super().__init__(obstacles)
         self.gt_checker = gt_checker if gt_checker is not None else CollisionChecker(self.obstacles)
-        self.initialize()
+        self.train_method = None
     
-    def initialize(self):
-        self.support_points = np.random.random((1000, 2)) * 10
-        self.gains = np.zeros(1000)
-        self.gamma = 30
-        K = np.tile(self.support_points[np.newaxis, :], (1000, 1, 1))
+    def initialize(self, num_init_points=100):
+        self.support_points = np.random.random((num_init_points, 2)) * 10
+        self.gains = np.zeros(num_init_points)
+        self.gamma = 3 #2*self.support_points.var()
+        K = np.tile(self.support_points[np.newaxis, :], (num_init_points, 1, 1))
+        # self.kernel_matrix = (self.support_points@self.support_points.T+1)**2
         self.kernel_matrix = 1/(1+self.gamma/2*np.sum((K-K.transpose(1, 0, 2))**2, axis=2))**2
         self.hypothesis = self.gains@self.kernel_matrix
         self.max_n_support = 200
         self.conditional_bias = 1
 
-    def train(self, max_iteration=1000):
+    def train(self, max_iteration=1000, method='original'):
+        self.train_method = method
+        if method == 'original':
+            self.train_original(max_iteration)
+        elif method == 'sgd':
+            self.train_sgd(max_iteration)
+        elif method == 'svm':
+            self.train_svm()
+        
+        self.support_points = self.support_points[self.gains != 0]
+        self.hypothesis = self.hypothesis[self.gains != 0]
+        self.y = self.y[self.gains != 0]
+        self.gains = self.gains[self.gains != 0]
+        print('{} training done'.format(method))
+        
+
+    def train_original(self, max_iteration=1000):
         self.y = np.zeros(len(self.support_points))
         for i in range(len(self.support_points)):
             self.y[i] = 1 if self.gt_checker.is_collision(self.support_points[i]) else -1
@@ -60,34 +80,136 @@ class Fastron(CollisionChecker):
                 delta_gain = self.conditional_bias**((1+self.y[min_margin_idx])/2) * self.y[min_margin_idx] - self.hypothesis[min_margin_idx]
                 self.gains[min_margin_idx] += delta_gain
                 self.hypothesis += delta_gain * self.kernel_matrix[min_margin_idx]
-        print('Training done')
+        
+    def train_sgd(self, max_iteration=1000):
+        self.y = np.zeros(len(self.support_points))
+        for i in range(len(self.support_points)):
+            self.y[i] = 1 if self.gt_checker.is_collision(self.support_points[i]) else -1
+        y = torch.tensor(self.y)
+        K = torch.tensor(self.kernel_matrix)
+        gains = torch.tensor(self.gains, requires_grad=True)
 
-
+        # self.grad = self.kernel_matrix@self.y
+        for it in range(max_iteration):
+            margin = (gains@K)*y
+            margin[margin > 0] = torch.log(1 + margin[margin > 0])
+            sum_margin = margin.sum()
+            gains.grad = None
+            sum_margin.backward()
+            gains.data += 0.001 * gains.grad
+            # self.gains /= np.linalg.norm(self.gains)
+        
+        self.gains = gains.detach().numpy()
+    
+    def train_svm(self):
+        self.y = np.zeros(len(self.support_points))
+        for i in range(len(self.support_points)):
+            self.y[i] = 1 if self.gt_checker.is_collision(self.support_points[i]) else -1
+        self.svm = SVC(C=1e4, kernel='rbf')
+        self.svm.fit(self.support_points, self.y)
+        # self.support_points = self.svm.support_vectors_
+        self.gains[self.svm.support_] = self.svm.dual_coef_.reshape(-1)
+        self.intercept = self.svm.intercept_
+        self.var = self.support_points.var()
+        # print('Intercept:', self.intercept)
+        # print('Gains: ', self.svm.dual_coef_)
+        
     def is_collision(self, point):
         return self.score(point) > 0
     
     def score(self, point):
+        if self.train_method == 'original':
+            return self.score_original(point)
+        elif self.train_method == 'sgd':
+            self.score_original(point)
+        elif self.train_method == 'svm':
+            self.score_svm(point)
+
+    def score_original(self, point):
         kernel_values = 1/(1+self.gamma/2*np.sum((self.support_points-point)**2, axis=1))**2
         score = self.gains@kernel_values
         return score
     
-    def vis(self, size=100):
-        if isinstance(size, int):
-            size = [size, size]
-        xx, yy = np.meshgrid(np.linspace(0, 10, size[0]), np.linspace(0, 10, size[1]))
-        grid_points = np.stack([xx, yy], axis=2).reshape((-1, 2))
-        grid_score = np.array(list(map(self.score, grid_points))).reshape((size[0], size[1]))
-        non_zero_gains = list(filter(lambda a: a != 0, self.gains))
-        real_support_points = self.support_points[self.gains != 0]
+    def score_nn(self, point):
+        dif_abs = np.abs((self.support_points-point))
+        dist = np.sqrt(np.sum(dif_abs**2, axis=1))
+        dist -= dist.min()
+        # print(dist.min())
+        kernel_values = 1/(1+self.gamma/2*dist**2)**2
+        # nn_idx = np.argmax(kernel_values)
+        # score = self.hypothesis[nn_idx] * (2-kernel_values[nn_idx])
+        score = self.gains@kernel_values
+        return score
+    
+    def score_svm(self, point):
+        kernel_values = np.exp(-np.sum((self.support_points-point)**2/2/self.var, axis=1))
+        return (self.gains)@kernel_values + self.intercept
+        # return self.svm.decision_function(point.reshape(1, -1))
+    
+def vis(model, size=100, seed=2019):
+    if isinstance(size, int):
+        size = [size, size]
+    xx, yy = np.meshgrid(np.linspace(0, 10, size[0]), np.linspace(0, 10, size[1]))
+    grid_points = np.stack([xx, yy], axis=2).reshape((-1, 2))
+    fig, ax = plt.subplots()
 
-        fig, ax = plt.subplots()
-        ax.set_title('number of support points = {}'.format(len(real_support_points)))
-        c = ax.pcolormesh(xx, yy, grid_score, cmap='RdBu', vmin=-2, vmax=2)
-        plt.scatter(real_support_points[:, 0], real_support_points[:, 1], marker='.', c='black')
-        plt.contour(xx, yy, grid_score, levels=1)
-        plt.axis('equal')
-        fig.colorbar(c, ax=ax)
-        plt.show()
+    np.random.seed(seed)
+    model.initialize(1000)
+    model.train(1000, method='original')
+    real_support_points = model.support_points
+    grid_score = np.fromiter(map(model.score_original, grid_points), np.float).reshape((size[0], size[1]))
+    ax1 = plt.subplot(1,3,1)
+    c = ax1.pcolormesh(xx, yy, grid_score, cmap='RdBu_r', vmin=-np.abs(grid_score).max(), vmax=np.abs(grid_score).max())
+    ax1.scatter(real_support_points[:, 0], real_support_points[:, 1], marker='.', c='black')
+    ax1.contour(xx, yy, (grid_score).astype(float), levels=0)
+    ax1.axis('equal')
+    fig.colorbar(c, ax=ax1)
+    sparse_score = grid_score[::10, ::10]
+    score_grad_x = -ndimage.sobel(sparse_score, axis=1)
+    score_grad_y = -ndimage.sobel(sparse_score, axis=0)
+    score_grad = np.stack([score_grad_x, score_grad_y], axis=2)
+    score_grad /= np.linalg.norm(score_grad, axis=2, keepdims=True)
+    score_grad_x, score_grad_y = score_grad[:, :, 0], score_grad[:, :, 1]
+    ax1.quiver(xx[::10, ::10], yy[::10, ::10], score_grad_x, score_grad_y, scale=30, color='red')
+    ax1.set_title('Original Fastron, no. of support points = {}'.format(len(real_support_points)))
+
+    grid_nn_score = np.fromiter(map(model.score_nn, grid_points), np.float).reshape((size[0], size[1]))
+    ax2 = plt.subplot(1,3,2)
+    c = ax2.pcolormesh(xx, yy, grid_nn_score, cmap='RdBu_r', vmin=-np.abs(grid_nn_score).max(), vmax=np.abs(grid_nn_score).max())
+    ax2.scatter(real_support_points[:, 0], real_support_points[:, 1], marker='.', c='black')
+    ax2.contour(xx, yy, (grid_nn_score).astype(float), levels=0)
+    ax2.axis('equal')
+    fig.colorbar(c, ax=ax2)
+    sparse_score = grid_nn_score[::10, ::10]
+    score_grad_x = -ndimage.sobel(sparse_score, axis=1)
+    score_grad_y = -ndimage.sobel(sparse_score, axis=0)
+    score_grad = np.stack([score_grad_x, score_grad_y], axis=2)
+    score_grad /= np.linalg.norm(score_grad, axis=2, keepdims=True)
+    score_grad_x, score_grad_y = score_grad[:, :, 0], score_grad[:, :, 1]
+    ax2.quiver(xx[::10, ::10], yy[::10, ::10], score_grad_x, score_grad_y, scale=30, color='red')
+    ax2.set_title('NN inference, no. of support points = {}'.format(len(real_support_points)))
+
+    np.random.seed(seed)
+    model.initialize(1000)
+    model.train(1000, method='svm')
+    real_support_points = model.support_points
+    grid_svm_score = np.fromiter(map(model.score_svm, grid_points), np.float).reshape((size[0], size[1]))
+    ax3 = plt.subplot(1,3,3)
+    c = ax3.pcolormesh(xx, yy, grid_svm_score, cmap='RdBu_r', vmin=-np.abs(grid_svm_score).max(), vmax=np.abs(grid_svm_score).max())
+    ax3.scatter(real_support_points[:, 0], real_support_points[:, 1], marker='.', c='black')
+    ax3.contour(xx, yy, (grid_svm_score).astype(float), levels=0)
+    ax3.axis('equal')
+    fig.colorbar(c, ax=ax3)
+    sparse_score = grid_svm_score[::10, ::10]
+    score_grad_x = -ndimage.sobel(sparse_score, axis=1)
+    score_grad_y = -ndimage.sobel(sparse_score, axis=0)
+    score_grad = np.stack([score_grad_x, score_grad_y], axis=2)
+    score_grad /= np.linalg.norm(score_grad, axis=2, keepdims=True)
+    score_grad_x, score_grad_y = score_grad[:, :, 0], score_grad[:, :, 1]
+    ax3.quiver(xx[::10, ::10], yy[::10, ::10], score_grad_x, score_grad_y, scale=30, color='red')
+    ax3.set_title('SVM, no. of support points={}'.format(len(model.support_points)))
+
+    plt.show()
 
 if __name__ == '__main__':
     obstacles = [
@@ -95,11 +217,4 @@ if __name__ == '__main__':
         ('rect', (5, 7), (5, 3))]
     obstacles = [Obstacle(*param) for param in obstacles]
     checker = Fastron(obstacles)
-    checker.train(1000)
-    # plt.axis('equal')
-    # plt.xlim((0, 10))
-    # plt.ylim((0, 10))
-    # plot_decision_regions(X=checker.support_points, y=checker.y.astype(np.integer), clf=checker, markers=[None], legend=None, filler_feature_ranges=[(0, 10), (0, 10)])
-    # plt.show()
-    # print(checker.support_points, checker.gains)
-    checker.vis(200)
+    vis(checker, 200)
---
+++ MultiFastron.py
@@ -0,0 +1,105 @@
+import numpy as np
+from Fastron import CollisionChecker, Obstacle, plt
+
+class MultiChecker:
+    def __init__(self, objects: [Obstacle,]):
+        self.objects = objects
+    
+    def predict(self, point):
+        return np.fromiter(map(lambda obj: 1 if obj.is_collision(point) else -1, self.objects), int)
+
+class MultiFastron(MultiChecker):
+    def __init__(self, objects, num_class, gt_checker=None):
+        super().__init__(obstacles)
+        self.gt_checker = gt_checker if gt_checker is not None else MultiChecker(self.objects)
+        self.num_class = num_class
+        self.initialize(1000)
+
+    def initialize(self, num_init_points=100):
+        self.support_points = np.random.random((num_init_points, 2)) * 10
+        self.gains = np.zeros((self.num_class, num_init_points))
+        self.gamma = 3
+        K = np.tile(self.support_points[np.newaxis, :], (num_init_points, 1, 1))
+        self.kernel_matrix = 1/(1+self.gamma/2*np.sum((K-K.transpose(1, 0, 2))**2, axis=2))**2
+        self.hypothesis = self.gains@self.kernel_matrix
+        self.max_n_support = 200
+        self.conditional_bias = 1
+    
+    def train_sgd(self, max_iteration=1000):
+        pass
+    
+    def train(self, max_iteration=1000, method='original'):
+        if method == 'original':
+            self.train_original(max_iteration)
+        elif method == 'sgd':
+            self.train_sgd(max_iteration)
+    
+    def train_original(self, max_iteration=1000):
+        self.y = np.array([self.gt_checker.predict(p) for p in self.support_points]).transpose()
+
+        print('Fastron training...')
+        for it in range(max_iteration):
+            margin = self.y * self.hypothesis
+            for class_margin, class_y, class_gain, class_hypo in zip(margin, self.y, self.gains, self.hypothesis):
+                min_margin_idx = np.argmin(class_margin)
+                if class_margin[min_margin_idx] <= 0:
+                    delta_gain = self.conditional_bias**((1+class_y[min_margin_idx])/2) * class_y[min_margin_idx] - class_hypo[min_margin_idx]
+                    class_gain[min_margin_idx] += delta_gain
+                    class_hypo += delta_gain * self.kernel_matrix[min_margin_idx]
+        valid = np.sum(self.gains != 0, axis=0)
+        self.support_points = self.support_points[valid != 0]
+        self.hypothesis = self.hypothesis[:, valid != 0]
+        self.gains = self.gains[:, valid != 0]
+        print('Training done')
+    
+    def predict(self, point):
+        score = self.score(point)
+        max_class_idx = np.argmax(score)
+        return max_class_idx+1 if score[max_class_idx] > 0 else 0
+        # return np.argmax(self.score(point))
+
+    def score(self, point):
+        kernel_values = 1/(1+self.gamma/2*np.sum((self.support_points-point)**2, axis=1))**2
+        score = self.gains@kernel_values
+        return score
+    
+    def vis(self, size=100):
+        if isinstance(size, int):
+            size = [size, size]
+        xx, yy = np.meshgrid(np.linspace(0, 10, size[0]), np.linspace(0, 10, size[1]))
+        grid_points = np.stack([xx, yy], axis=2).reshape((-1, 2))
+        # grid_score = np.fromiter(map(lambda p: self.score(p)[0], grid_points), np.float64).reshape((size[0], size[1]))
+        grid_score = np.fromiter(map(self.predict, grid_points), np.float64).reshape((size[0], size[1]))
+        # grid_nn_score = np.array(list(map(self.score_nn, grid_points))).reshape((size[0], size[1]))
+        real_support_points = self.support_points#[self.gains != 0]
+
+        fig, ax = plt.subplots()
+        ax.set_title('number of support points = {}'.format(len(real_support_points)))
+
+        c = ax.pcolormesh(xx, yy, grid_score, cmap='RdBu_r', vmin=-2, vmax=2)#, vmin=self.hypothesis.min(), vmax=self.hypothesis.max()) # vmin=-2, vmax=2)
+        ax.scatter(real_support_points[:, 0], real_support_points[:, 1], marker='.', c='black')
+        ax.contour(xx, yy, (grid_score>0).astype(float), levels=1)
+        ax.axis('equal')
+        fig.colorbar(c, ax=ax)
+
+        # ax2 = plt.subplot(1,2,2)
+        # c = ax2.pcolormesh(xx, yy, grid_nn_score, cmap='RdBu', vmin=-2, vmax=2)# , vmin=self.hypothesis.min(), vmax=self.hypothesis.max()) # vmin=-2, vmax=2)
+        # ax2.scatter(real_support_points[:, 0], real_support_points[:, 1], marker='.', c='black')
+        # ax2.contour(xx, yy, (grid_nn_score>0).astype(float), levels=1)
+        # ax2.axis('equal')
+        # fig.colorbar(c, ax=ax2)
+
+        plt.show()
+
+
+if __name__ == '__main__':
+    obstacles = [
+        ('circle', (2, 1), 1.5),
+        ('rect', (5, 7), (5, 3))]
+    obstacles = [Obstacle(*param) for param in obstacles]
+
+    np.random.seed(1314)
+    classifier = MultiFastron(obstacles, len(obstacles))
+    classifier.train(1000)
+    print(classifier.gains, classifier.gains.size)
+    classifier.vis(200)
