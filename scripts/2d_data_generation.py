a33a4e357616f45f87414f45dd29c05df0fd24df
Evan Smith <eps001@ucsd.edu>
2022-04-12 04:20:21 +0000

    Make obstacles a constant

--- scripts/2d_data_generation.py
+++ scripts/2d_data_generation.py
@@ -6,6 +6,50 @@ from diffco.model import RevolutePlanarRobot
 from generate_batch_data_2d import generate_one
 
 
+obstacles = {
+    # ('circle', (3, 2), 2), #2circle
+    # ('circle', (-2, 3), 0.5), #2circle
+    # ('rect', (-2, 3), (1, 1)),
+    # ('rect', (1.7, 3), (2, 3)),
+    # ('rect', (-1.7, 3), (2, 3)),
+    # ('rect', (0, -1), (10, 1)),
+    # ('rect', (8, 7), 1),
+    '1rect_1circle': [('rect', (4, 3), (2, 2)),
+        ('circle', (-4, -3), 1)],
+    # ('rect', (4, 3), (2, 2)), # 2rect
+    # ('rect', (-4, -3), (2, 2)) # 2rect
+    # ('rect', (3, 2), (2, 2)) # 1rect
+    '3circle': [
+        ('circle', (0, 4.5), 1), #3circle
+        ('circle', (-2, -3), 2), #3circle
+        ('circle', (-2, 2), 1.5), #3circle
+    ],
+    '1rect_1circle_7d': [
+        ('circle', (-2, 3), 1), #1rect_1circle_7d
+        ('rect', (3, 2), (2, 2)) #1rect_1circle_7d
+    ],
+    '2class_1': [
+        ('rect', (5, 0), (2, 2), 0), #2class_1
+        ('circle', (-3, 6), 1, 1), #2class_1
+        ('rect', (-5, 2), (2, 1.5), 1), #2class_1
+        ('circle', (-5, -2), 1.5, 1), #2class_1
+        ('circle', (-3, -6), 1, 1) #2class_1
+    ],
+    '2class_2': [
+        ('rect', (0, 3), (16, 0.5), 1), #2class_2
+        ('rect', (0, -3), (16, 0.5), 0), #2class_2
+    ],
+    # ('rect', (-7, 3), (2, 2)) #1rect_active
+    '3circle_7d': [
+        ('circle', (-2, 2), 1), #3circle_7d
+        ('circle', (-3, 3), 1), #3circle
+        ('circle', (-6, -3), 1) #3circle
+    ]
+    # ('rect', (5, 4), (4, 4), 0), #2instance_big
+    # ('circle', (-5, -4), 2, 1) #2instance_big
+}
+
+
 def main(
         env_name: str = '3d_halfnarrow',
         folder: str = 'data/landscape',
@@ -18,48 +62,6 @@ def main(
     torch.manual_seed(random_seed)
     np.random.seed(random_seed)
 
-    obstacles = {
-        # ('circle', (3, 2), 2), #2circle
-        # ('circle', (-2, 3), 0.5), #2circle
-        # ('rect', (-2, 3), (1, 1)),
-        # ('rect', (1.7, 3), (2, 3)),
-        # ('rect', (-1.7, 3), (2, 3)),
-        # ('rect', (0, -1), (10, 1)),
-        # ('rect', (8, 7), 1),
-        '1rect_1circle': [('rect', (4, 3), (2, 2)),
-            ('circle', (-4, -3), 1)],
-        # ('rect', (4, 3), (2, 2)), # 2rect
-        # ('rect', (-4, -3), (2, 2)) # 2rect
-        # ('rect', (3, 2), (2, 2)) # 1rect
-        '3circle': [
-            ('circle', (0, 4.5), 1), #3circle
-            ('circle', (-2, -3), 2), #3circle
-            ('circle', (-2, 2), 1.5), #3circle
-        ],
-        '1rect_1circle_7d': [
-            ('circle', (-2, 3), 1), #1rect_1circle_7d
-            ('rect', (3, 2), (2, 2)) #1rect_1circle_7d
-        ],
-        '2class_1': [
-            ('rect', (5, 0), (2, 2), 0), #2class_1
-            ('circle', (-3, 6), 1, 1), #2class_1
-            ('rect', (-5, 2), (2, 1.5), 1), #2class_1
-            ('circle', (-5, -2), 1.5, 1), #2class_1 
-            ('circle', (-3, -6), 1, 1) #2class_1
-        ],
-        '2class_2': [
-            ('rect', (0, 3), (16, 0.5), 1), #2class_2
-            ('rect', (0, -3), (16, 0.5), 0), #2class_2
-        ],
-        # ('rect', (-7, 3), (2, 2)) #1rect_active
-        '3circle_7d': [
-            ('circle', (-2, 2), 1), #3circle_7d
-            ('circle', (-3, 3), 1), #3circle
-            ('circle', (-6, -3), 1) #3circle
-        ]
-        # ('rect', (5, 4), (4, 4), 0), #2instance_big
-        # ('circle', (-5, -4), 2, 1) #2instance_big
-    }
     if env_name == '7d_narrow':
         obstacles = []
         lb = np.array([-8, 1.0], dtype=float)
