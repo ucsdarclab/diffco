79bdfdc5bbf3c6b9311855715c09ff24b06917b7
Yuheng Zhi <yzhi@ucsd.edu>
2024-04-08 18:26:22 -0700

    fix imports to new structure

--- diffco/collision_interfaces/__init__.py
+++ diffco/collision_interfaces/__init__.py
@@ -1,3 +1,4 @@
 from .env_interface import ShapeEnv, PCDEnv
-from .urdf_interface import RobotInterfaceBase, URDFRobot, MultiURDFRobot, robot_description_folder
+from .urdf_interface import RobotInterfaceBase, URDFRobot, MultiURDFRobot, robot_description_folder, \
+    FrankaPanda, KUKAiiwa, TwoLinkRobot, TrifingerEdu
 from .ros_interface import ROSRobotEnv
